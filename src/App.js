baa9e58e3857fd3efae0fd15de4b4f9664bd1eda
SaunakMalaSaunakMalla7 <SaunakMalaSaunakMalla7@gmail.com>
2023-08-13 21:53:47 +0545

    Add Del for todo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { AiOutlineDelete } from "react-icons/ai";
 import { BsCheckLg } from "react-icons/bs";
 
 function App() {
   const [isCompleteScree, setIsCompleteScreen] = useState(false);
+  const [allTodos, setAllTodos] = useState([]);
+  const [newTodoTitle, setNewTodoTitle] = useState("");
+
+  const handleAddNewToDo = () => {
+    const newToDoObj = {
+      title: newTodoTitle,
+    };
+    console.log(newToDoObj);
+    let updatedTodoArr = [...allTodos];
+    updatedTodoArr.push(newToDoObj);
+    // console.log (updatedTodoArr);
+    setAllTodos(updatedTodoArr);
+    localStorage.setItem("todolist", JSON.stringify(updatedTodoArr));
+    setNewTodoTitle("");
+  };
+  useEffect(() => {
+    let savedTodos = JSON.parse(localStorage.getItem("todolist"));
+    if (savedTodos) {
+      setAllTodos(savedTodos);
+    }
+  }, []);
+
+const handleDeleteTodo=(index)=>{
+const deleteTodo = [...allTodos]
+deleteTodo.splice(index, 1);
+localStorage.setItem("todolist", JSON.stringify(deleteTodo))
+setAllTodos(deleteTodo)
+}
+  
   return (
     <div className="todo-wrapper">
       <h1> Todos </h1>
       <div className="todo-input">
         <div className="todo-input-item">
           <label>Title</label>
-          <input type="text" placeholder="what's the tast"></input>
+          <input
+            type="text"
+            value={newTodoTitle}
+            onChange={(e) => setNewTodoTitle(e.target.value)}
+            placeholder="what's the tast"
+          ></input>
         </div>
-        <div className="todo-input-item">
+        {/* <div className="todo-input-item">
           <label>Catagory</label>
           <input type="text" placeholder="Whats the tast"></input>
-        </div>
+        </div> */}
         <div className="todo-input-item">
-          <button type="button" className="primary-btn">
+          <button
+            type="button"
+            onClick={handleAddNewToDo}
+            className="primary-btn"
+          >
             Add
           </button>
         </div>
@@ -40,13 +78,18 @@ function App() {
         </button>
       </div>
       <div className="todo-list">
-        <div className="todo-list-item">
-          <h1>task 1</h1>
-          <div>
-            <AiOutlineDelete className="icon" />
+        {allTodos.map((item, index) => (
+          <div className="todo-list-item" key={index}>
+            <div>
+              <h3>{item.title}</h3>
+
+            </div>
+            <div>
+              <AiOutlineDelete  className="icon" onClick={()=>handleDeleteTodo(index)}/>
+              <BsCheckLg className=" check-icon" />
+            </div>
           </div>
-          <BsCheckLg className=" check-icon" />
-        </div>
+        ))}
       </div>
     </div>
   );
